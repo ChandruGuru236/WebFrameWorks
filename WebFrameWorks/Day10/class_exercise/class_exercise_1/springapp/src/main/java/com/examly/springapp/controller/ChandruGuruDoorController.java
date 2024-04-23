f0692afb2d16533184a0df4c02abae8ef5afa6a9
ChandruGuru236 <124863545+ChandruGuru236@users.noreply.github.com>
2024-04-23 22:59:26 +0530

    Add files via upload

---
+++ WebFrameWorks/Day10/class_exercise/class_exercise_1/springapp/src/main/java/com/examly/springapp/controller/ChandruGuruDoorController.java
@@ -0,0 +1,101 @@
+package com.examly.springapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.model.ChandruGuruDoor;
+import com.examly.springapp.service.ChandruGuruDoorService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api")
+public class ChandruGuruDoorController {
+
+    @Autowired
+    public ChandruGuruDoorService doorService;
+
+    @PostMapping("/door")
+    public ResponseEntity<ChandruGuruDoor> postData(@RequestBody ChandruGuruDoor door) {
+        ChandruGuruDoor doorData = doorService.postData(door);
+        if (doorData != null) {
+            return new ResponseEntity<>(doorData, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("door/bycolor/{color}")
+    public ResponseEntity<List<ChandruGuruDoor>> getBycolor(@PathVariable String color) {
+        List<ChandruGuruDoor> door = doorService.getBycolor(color);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("door/{doorid}")
+    public ResponseEntity<ChandruGuruDoor> updateDetailResponseEntity(@PathVariable int doorid, @RequestBody ChandruGuruDoor door) {
+        ChandruGuruDoor doorData = doorService.updateDetail(doorid, door);
+        if (doorData != null) {
+            return new ResponseEntity<>(doorData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("door/{doorid}")
+    public ResponseEntity<String> deleteDoor(@PathVariable int doorid) {
+        if (doorService.deleteDoor(doorid)) {
+            return new ResponseEntity<>("Door deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/door/bydoortype/{doortype}")
+    public ResponseEntity<List<ChandruGuruDoor>> getByDoor(@PathVariable String doortype) {
+        List<ChandruGuruDoor> door = doorService.getByDoorType(doortype);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/door")
+    public ResponseEntity<List<ChandruGuruDoor>> getAllDetails() {
+        List<ChandruGuruDoor> door = doorService.getAllDetails();
+        if (door != null) {
+
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+    @GetMapping("/door/{doorid}")
+    public ResponseEntity<ChandruGuruDoor> getDetailsById(@PathVariable int doorid) {
+        ChandruGuruDoor door = doorService.getDetailsById(doorid);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
